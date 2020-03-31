5f0b77ff5841bab1b187f72e523b5dddd0e7196c
chomarlay <chomarlay.aung@gmail.com>
2020-04-01 05:03:48 +1100

    Section 9 - User Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
-const Login = () => {
+const Login = props => {
+  const authContext = useContext(AuthContext);
+  const { login, clearErrors, error, isAuthenticated } = authContext;
+
+  const alertContext = useContext(AlertContext);
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     email: '',
     password: ''
   });
   const { email, password } = user;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      // if authenticated route to the home page after the login
+      props.history.push('/');
+    }
+    if (error === 'Invalid Credentials') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
 
   const onChange = e => {
     setUser({ ...user, [e.target.name]: e.target.value });
@@ -13,7 +33,11 @@ const Login = () => {
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log('Login User');
+    if (email === '' || password === '') {
+      setAlert('Please enter all fields.', 'danger');
+    } else {
+      login({ email, password });
+    }
   };
   return (
     <div className='form-container'>

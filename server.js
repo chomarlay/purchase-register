e8d0a724d465e759aa45376255d2f468776fcb5e
chomarlay <chomarlay.aung@gmail.com>
2020-06-13 15:02:28 +1000

    Heroku deployment set up

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -17,6 +18,16 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/purchases', require('./routes/purchases'));
 app.use('/api/attachments', require('./routes/attachments'));
 
+// Serve static assets in production -- to deploy in Heroku
+//NOTE:  this has to be after the above Define Routes
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder -- serve client
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
